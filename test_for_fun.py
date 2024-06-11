f2b59cc542dc73ca62e40acc94a8971754d89186
chenpolly <105775304+chenpolly@users.noreply.github.com>
2024-06-11 17:38:29 +0800

    Add files via upload
    心理測驗，練習 if 及 input 的使用

---
+++ test_for_fun.py
@@ -0,0 +1,145 @@
+question1 = input("世界末日來了, 如果你只能解救一種動物, 你會救以下哪一個？ 1.兔 2.羊 3.鹿 4.馬 (請輸入選項數字: ")
+question1 =int(question1)
+print(f'你選的是{question1}')
+if question1 == 1:
+    print("兔")
+    ans1="外冷內熱的人"
+elif question1== 2:
+    print("羊")
+    ans1="溫暖的人"
+elif question1== 3:
+    print("鹿")
+    ans1 ="優雅有禮貌的人"
+elif question1== 4:
+    print("馬")
+    ans1 = "不受拘束、响往自由的人"
+else:
+    print("請重新選擇")
+
+question2 = input("你到一個部落拜訪，部落首領堅持讓你選一種動物帶回去作為紀念，你會選？ 1.狼 2.獅子 3.蛇 4.長頸鹿 (請輸入選項數字:")
+question2 = int(question2)
+print(f"你選擇的是{question2}")
+if question2 == 1:
+    print("狼")
+    ans2 ="有創造性、新鮮感，讓你不會感到無趣"
+elif question2== 2:
+    print("獅子")
+    ans2 ="直來直往、直接表達愛意"
+elif question2== 3:
+    print("蛇")
+    ans2 ="忽冷忽熱、游移不定"
+elif question2== 4:
+    print("長頸鹿")
+    ans2 ="有耐心，對你永不放棄"
+else:
+    print("請重新選擇")
+
+question3 = input("你做錯了事，上天懲罰你變成人以外的動物，你想變成？ 1.狗 2.貓 3.馬 4.蛇 (請輸入選項數字: ")
+question3 = int(question3)
+print(f"你選擇的是{question3}")
+if question3 == 1:
+    print("狗")
+    ans3 = "忠實、忠誠的"
+elif question3== 2:
+    print("貓")
+    ans3 = "有個性的"
+elif question3== 3:
+    print("馬")
+    ans3 ="樂觀的"
+elif question3== 4:
+    print("蛇")
+    ans3 = "有包容心的"
+else:
+    print("請重新選擇")
+
+question4 = input("你有能力可以使某種動物消失，你會選擇哪一種讓它消失？ 1.獅子 2.蛇 3.鱷魚 4.鯊魚 (請輸入選項數字:")
+question4 = int(question4)
+print(f"你選擇的是{question4}")
+if question4 == 1:
+    print("獅子")
+    ans4 ="傲慢自大、像獨裁者"
+elif question4== 2:
+    print("蛇")
+    ans4 ="情緒化、喜怒無常"
+elif question4== 3:
+    print("鱷魚")
+    ans4 ="無情、冷血又愛諷刺人"
+elif question4== 4:
+    print("鯊魚")
+    ans4 ="讓你沒有安全感"
+else:
+    print("請重新選擇")
+
+question5 = input("你遇見了一種會說人話的動物，你認為會是哪一種動物？ 1.羊 2.馬 3.兔 4.鳥 (請輸入選項數字: ")
+question5 = int(question5)
+print(f"你選擇的是{question5}")
+if question5 == 1:
+    print("羊")
+    ans5 ="不用多說就知道對方在想什麼、有心連心的感覺"
+elif question5== 2:
+    print("馬")
+    ans5 = "可以討論任何事情，沒有秘密"
+elif question5== 3:
+    print("兔")
+    ans5 = "一段讓彼此能一直感受到溫暖與愛戀的關係"
+elif question5== 4:
+    print("鳥")
+    ans5 = "不只關心現在也關心將來，能一起成長的長期關係"
+else:
+    print("請重新選擇")
+
+question6 = input("你置身在一個孤島上，如果只能選一種動物來陪你，你會選哪一種？ 1.狗 2.豬 3.母牛 4.鳥 (請輸入選項數字: ")
+question6 = int(question6)
+print(f"你選擇的是{question6}")
+if question6 == 1:
+    print("狗")
+    ans6 = "重視社會與道德規範，很難做這樣的事"
+elif question6== 2:
+    print("豬")
+    ans6 = "對慾望比較無法抗拒，可能會越軌"
+elif question6== 3:
+    print("母牛")
+    ans6 = "不會主動但不善拒絕，會視著努力讓自己不這樣做"
+elif question6== 4:
+    print("鳥")
+    ans6 = "不夠堅定、不太適合婚姻，也不想做承諾"
+else:
+    print("請重新選擇")
+
+question7 = input("今天你擁有可以馴服所有動物的能力，你會選擇哪一種動物當寵物？ 1.恐龍 2.白老虎 3.北極熊 4.豹 (請輸入選項數字: ")
+question7 = int(question7)
+print(f"你選擇的是{question7}")
+if question7 == 1:
+    print("恐龍")
+    ans7 = "非常消極，不認為有快樂婚姻的存在"
+elif question7== 2:
+    print("白老虎")
+    ans7 = "認為婚姻很珍貴，一旦結婚，你會很真心婚姻與伴侶"
+elif question7== 3:
+    print("北極熊")
+    ans7 = "害怕婚姻，覺得會被剝奪自由"
+elif question7== 4:
+    print("豹")
+    ans7 = "很想結婚，但事實上你不知道婚姻是什麼"
+else:
+    print("請重新選擇")
+
+question8 = input("你可以擁有5分鐘的時間去當一種動物，你會選擇當哪一種？ 1.獅子 2.貓 3.馬 4.鴿子 (請輸入選項數字: ")
+question8 = int(question8)
+print(f"你選擇的是{question8}")
+if question8 == 1:
+    print("獅子")
+    ans8 = "渴望愛情，願意為對方付出，但不會輕易愛上一個人"
+elif question8== 2:
+    print("貓")
+    ans8 = "自我中心，認為愛情可以輕易得到，也可以輕易放下"
+elif question8== 3:
+    print("馬")
+    abs8 = "不想被固定的關係綁住，比較享受處處調情的樂趣"
+elif question8== 4:
+    print("鴿子")
+    ans8 = "認為愛情是兩個人相互的承諾"
+else:
+    print("請重新選擇")
+
+print(f"你在現實生活中，容易被{ans1}吸引。{ans2}的追求手段最容易讓你情不自禁！你希望給愛人{ans3}印象。你最討厭{ans4}，會因此分手。 你希望跟伴侶建立一個{ans5}。你外遇或是劈腿的可能性是{ans6}。你的婚姻的看法是{ans7}。此刻你對愛情的看法是{ans8}")    
